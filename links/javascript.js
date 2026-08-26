7d532f128420357f6e14ca463f91fabd7ee51cef
ChrysX6 <165295828+ChrysX6@users.noreply.github.com>
2026-08-26 09:02:47 -0300

    Update javascript.js

--- links/javascript.js
+++ links/javascript.js
@@ -96,6 +96,7 @@ const systemsDB = [
     { name:"FECHAMENTO SN", desc:"Fechamento Fiscal - SN", url:"https://chrysx6.github.io/Controle-Fiscal-SN/", icon:"fa-solid fa-solidLarge fa-chart-pie", tags:["fiscal","fechamento","declaracoes",] },
     { name:"FECHAMENTO LP/LR", desc:"Fechamento Fiscal - LP/LR", url:"https://chrysx6.github.io/Controle-Fiscal/", icon:"fa-solid fa-solidLarge fa-chart-pie", tags:["fiscal","fechamento","declaracoes",] },
     { name:"ANIVERSARIOS EMPRESAS", desc:"Site para visualizar os aniversários das empresas", url:"https://chrysx6.github.io/AniversariosEmpresas/", icon:"fa-solid fa-cake-candles", tags:["Martins","empresas","Aniversários"] },
+    { name:"ANIVERSARIOS CLIENTES", desc:"Site para visualizar os aniversários dos clientes", url:"https://chrysx6.github.io/AniversariosClientes/", icon:"fa-solid fa-cake-candles", tags:["Martins","clientes","Aniversários"] },
     { name:"CONSULTA OPTANTES SIMPLES", desc:"Consultar empresas optantes pelo Simples Nacional", url:"https://consopt.www8.receita.fazenda.gov.br/consultaoptantes", icon:"fas fa-users", tags:["simples nacional","consulta","optantes"] },
     { name:"CONSULTA CNPJ", desc:"Consulta de CNPJ da Receita Federal", url:"https://solucoes.receita.fazenda.gov.br/servicos/cnpjreva/cnpjreva_solicitacao.asp", icon:"fas fa-database", tags:["cnpj","consulta","receita federal"] },
     { name:"JUCESP", desc:"Junta Comercial do Estado de São Paulo", url:"https://www.jucesponline.sp.gov.br/", icon:"fas fa-landmark", tags:["junta comercial","sp","empresas"] },
